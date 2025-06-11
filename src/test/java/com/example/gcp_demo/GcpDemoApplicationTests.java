d3f84ef5842b8167fd90066f5f4afd88b739c1d9
sreenath <rdgowda79@gmail.com>
2025-06-11 10:52:05 +0530

    First commit

---
+++ src/test/java/com/example/gcp_demo/GcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
